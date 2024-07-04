aa2896bc0fddbc760d49bd63a4a947f4c98051f2
snowdream <sn0wdr1am@qq.com>
2024-07-04 20:32:46 +0800

    feat: WEBUI_PORT

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM snowdreamtech/alpine:3.20.0
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
-ENV WEBUI_PORT=8080 \
+ENV WEBUI_PORT=8112 \
     TORRENTING_PORT=6881 \
     WEBUI_USER="" \
     WEBUI_PASS="" \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,11 @@ if [ -n "${WEBUI_LANG}" ]; then
     sed -i "s|\"language\":.*|\"language\": \"${WEBUI_LANG}\",|g" /var/lib/deluge/config/web.conf
 fi
 
+# set WEBUI_PORT
+if [ -n "${WEBUI_PORT}" ]; then
+    sed -i "s|\"port\":.*|\"port\": \"${WEBUI_PORT}\",|g" /var/lib/deluge/config/web.conf
+fi
+
 # Deluge Bittorrent Client Web Interface
 /usr/bin/deluge-web --config  /var/lib/deluge/config -p 8112
 
