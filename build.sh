def7788dc05ce6d7a8b56fc62186dbd0ea5cbca1
snowdream <sn0wdr1am@qq.com>
2024-07-20 00:00:33 +0800

    feat: shell format

--- build.sh
+++ build.sh
@@ -5,13 +5,11 @@ DOCKER_HUB_PROJECT=snowdreamtech/deluge
 GITHUB_PROJECT=ghcr.io/snowdreamtech/deluge
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
--t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:2.1.1\
--t ${DOCKER_HUB_PROJECT}:2.1 \
--t ${DOCKER_HUB_PROJECT}:2 \
--t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:2.1.1\
--t ${GITHUB_PROJECT}:2.1 \
--t ${GITHUB_PROJECT}:2 \
-. \
---push
+    -t ${DOCKER_HUB_PROJECT}:latest \
+    -t ${DOCKER_HUB_PROJECT}:2.1.1-t ${DOCKER_HUB_PROJECT}:2.1 \
+    -t ${DOCKER_HUB_PROJECT}:2 \
+    -t ${GITHUB_PROJECT}:latest \
+    -t ${GITHUB_PROJECT}:2.1.1-t ${GITHUB_PROJECT}:2.1 \
+    -t ${GITHUB_PROJECT}:2 \
+    . \
+    --push
